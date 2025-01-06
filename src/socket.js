c5fd06ca8c9c9bcde1862725a9a1b6be0372b8ea
jai <jai@pearlorganisation.com>
2025-01-06 11:35:45 +0530

    updates

--- src/socket.js
+++ src/socket.js
@@ -3,10 +3,10 @@ import { io } from "socket.io-client";
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
   import.meta.env.VITE_REACT_APP_WORKING_ENVIRONMENT === "development"
-    ? import.meta.env.VITE_REACT_APP_API_BASE_URL_DEVELOPMENT
-    : import.meta.env.VITE_REACT_APP_API_BASE_URL_MAIN_PRODUCTION;
+    ? import.meta.env.VITE_REACT_APP_SOCKET_BASE_URL_DEVELOPMENT
+    : import.meta.env.VITE_REACT_APP_SOCKET_BASE_URL_MAIN_PRODUCTION;
 
-export const socket = io("http://localhost:3000", {
+export const socket = io(URL, {
   autoConnect: false,
   transports: ['websocket'], // Enforce WebSocket transport
   reconnectionAttempts: 3,  // Limit reconnection attempts
